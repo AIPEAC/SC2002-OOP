206a150abf8b7158d7a32d9c08cca6c29cc9d38c
ALPEAC <gptalnum@gmail.com>
2025-10-31 19:30:39 +0800

    reg

--- Code/Control/AuthenticationControl.java
+++ Code/Control/AuthenticationControl.java
@@ -2,11 +2,36 @@ package Control;
 import Entity.Users.*;
 
 public class AuthenticationControl {
+    private User currentUser=null;
     public AuthenticationControl(){
 
     }
     public void setLoggedin(User user){
+        currentUser=user;
+    }
+    public User getUser(){
+        return currentUser;
+    }
+    public String getUserIdentity(){
+        
+        try {
+            if (currentUser==null){
+                throw new Exception("bug: Authentication.java : getUserIdentity() Not Supposed to access getUserIdentity without a valid login");
+            }else if (currentUser instanceof CareerStaff){
+                return "CareerStaff";
+            }else if (currentUser instanceof Student){
+                return "Student";
+            }else if (currentUser instanceof CompanyRepresentative){
+                return "CompanyRepresentative";
+            }else{
+                throw new Exception("bug: Authentication.java : getUserIdentity() found that a User exist but ");
+            }
+            
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+            return null;
+        }
+        
         
     }
-
 }
