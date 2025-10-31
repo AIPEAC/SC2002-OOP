b984deb9f7e0be40edd0ba835e29f80115f37907
ALPEAC <gptalnum@gmail.com>
2025-10-31 16:52:50 +0800

    reg

--- Code/Boundary/CareerStaffCLI.java
+++ Code/Boundary/CareerStaffCLI.java
@@ -4,7 +4,7 @@ import Control.*;
 
 
 public class CareerStaffCLI implements Interface.InterfaceCLI{
-    CareerStaffCLI(LoginControl loginCtrl, IntershipOpportunityControl oppCtrl){
+    CareerStaffCLI(LoginControl loginCtrl, AuthenticationControl authCtrl, Intership oppCtrl){
         
     }
     @Override
