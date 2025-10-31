f5228eb4c22346227f5dfc658f050fa43afc10c5
ALPEAC <gptalnum@gmail.com>
2025-10-31 21:35:25 +0800

    diagram

--- Code/Entity/UserDirectory.java
+++ Code/Entity/UserDirectory.java
@@ -2,31 +2,44 @@ package Entity;
 
 import Entity.Users.*;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class UserDirectory {
-    private User currentUser;
+    
     private List<String[]> loginList;
     
+    public UserDirectory() {
+        loadLoginListFromDB();
+    }
 
     private void loadLoginListFromDB(){
         String csvFile = "Code/Lib/login_list.csv";
+        File file = new File(csvFile);
         String line = "";
         String cvsSplitBy = ",";
         loginList = new ArrayList<>();
 
-        try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
-
-            br.readLine(); // Skip header
-
-            while ((line = br.readLine()) != null) {
-                String[] loginData = line.split(cvsSplitBy);
-                loginList.add(loginData);
+        try {
+            if (!file.exists()) {
+                file.getParentFile().mkdirs(); 
+                file.createNewFile();
+                try (FileWriter writer = new FileWriter(file)) {
+                    writer.append("userID,passwordHash,salt\n");
+                }
             }
 
+            try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
+                br.readLine(); // Skip header
+                while ((line = br.readLine()) != null) {
+                    String[] loginData = line.split(cvsSplitBy);
+                    loginList.add(loginData);
+                }
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -35,23 +48,22 @@ public class UserDirectory {
     /*
      * the following 3 function is supposed to use only after the user login 
      * and we know which user they are
+     * e.g. we only create a student instance if the userID belongs to 
      * so the memory would not blow up if there are too many of them
      */
 
 
-    private void loadStudentListFromDB(String userID){
+    private void loadStudentFromDB(String userID){
         //
     }
-    private void loadStaffListFromDB(String userID){
+    private void loadStaffFromDB(String userID){
         //
     }
-    private void loadCompanyRepListFromDB(String userID){
+    private void loadCompanyRepFromDB(String userID){
         //
     }
     
-    public UserDirectory() {
-        loadLoginListFromDB();
-    }
+    
     public User verifyUser(String userID, String password){
         
         return null;
