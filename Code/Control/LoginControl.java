3936be82fb9553f09b334dadf6225f4fc8ca69ee
ALPEAC <gptalnum@gmail.com>
2025-10-31 17:23:47 +0800

    update attr name

--- Code/Control/LoginControl.java
+++ Code/Control/LoginControl.java
@@ -1,5 +1,18 @@
 package Control;
 //when registered, the password is set to "password". Remind them to change.
+
+import Entity.UserDirectory;
+
 public class LoginControl {
-    
+    private AuthenticationControl authCtrl;
+    private UserDirectory userDir;
+
+    public LoginControl(AuthenticationControl authCtrl, UserDirectory userDir) {
+        this.authCtrl = authCtrl;
+        this.userDir = userDir;
+    }
+
+    public void handleLogin(String userID, String password){
+        
+    }
 }
