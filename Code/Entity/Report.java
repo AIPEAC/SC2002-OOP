bc3c92d81a5af02e9eef5a78beb7eb955d925898
ALPEAC <gptalnum@gmail.com>
2025-11-01 02:41:39 +0800

    regular

--- Code/Entity/Report.java
+++ Code/Entity/Report.java
@@ -1,5 +1,22 @@
 package Entity;
+import java.util.List;
+
+import Entity.InternshipOpportunity;
+
 
 public class Report {
-    
+    private int reportIndex;
+    private List<InternshipOpportunity> internshipOpportunities;
+
+    public Report(int reportIndex,List<InternshipOpportunity> internshipOpportunities) {
+        this.reportIndex = reportIndex;
+        this.internshipOpportunities = internshipOpportunities;
+    }
+    public void formatOutput(){
+        List<String> comprehensiveDataList=comprehensiveDataList(internshipOpportunities);
+
+    }
+    private List<String> comprehensiveDataList(List<InternshipOpportunity> internshipOpportunities){
+        return null;
+    }
 }
