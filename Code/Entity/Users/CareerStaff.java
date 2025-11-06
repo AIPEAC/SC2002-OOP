1fa62b7206f3e7c18ab077cfe84e93753c602880
ALPEAC <gptalnum@gmail.com>
2025-11-06 14:07:10 +0800

    suppress warnings for unused role field in CareerStaff class

--- Code/Entity/Users/CareerStaff.java
+++ Code/Entity/Users/CareerStaff.java
@@ -2,6 +2,7 @@ package Entity.Users;
 
 public class CareerStaff extends User {
     private String department;
+    @SuppressWarnings("unused")
     private String role;
 
     public CareerStaff(String userID, String name, String email, String department, String role) {
