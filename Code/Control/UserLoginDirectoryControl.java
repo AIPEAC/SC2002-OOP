1733926f8b3dd725f2e2b8e55959253908b6da9a
ALPEAC <gptalnum@gmail.com>
2025-11-08 05:44:21 +0800

    fix(UserLoginDirectoryControl): adding loading function to constuctor

--- Code/Control/UserLoginDirectoryControl.java
+++ Code/Control/UserLoginDirectoryControl.java
@@ -22,6 +22,7 @@ public class UserLoginDirectoryControl{
     private boolean haveInitialized=false;
     
     public UserLoginDirectoryControl() {
+        loadUsersFromExamplesToDB();
         loadLoginListFromDB();
     }
 
